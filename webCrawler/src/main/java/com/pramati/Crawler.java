510f9ab65b6150d5b143816badf735600f0a3c02
RamaPramati <ramakrishna.saparapu@imaginea.com>
2015-04-15 19:04:57 +0530

    Design changed

--- webCrawler/src/main/java/com/pramati/Crawler.java
+++ webCrawler/src/main/java/com/pramati/Crawler.java
@@ -1,4 +1,4 @@
-package com.pramati.crawling;
+package com.pramati;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -10,43 +10,36 @@ import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class Crawler extends Client
+
+public class Crawler 
 {	
-	java.util.List<?> targetURLs;
 	static URL url;
 	String year;
-	HtmlPage currentPage;
 	
 	public Crawler(String url, String year) throws FailingHttpStatusCodeException, MalformedURLException, IOException
 	{
-	    currentPage=webClient.getPage(url);
         this.year=year;
         Crawler.url=new URL(url);
 	}
 
 	public void getMails() throws MalformedURLException, IOException
 	{
-		targetURLs = currentPage.getByXPath("//a[@href]");
-		Iterator<?> ir=targetURLs.iterator();
+		Filter checkCondition= new Filter();
+		java.util.List<String> storeURLs=checkCondition.isRequired(url);
+		Iterator<?> ir=storeURLs.iterator();
 		while(ir.hasNext())
       {
         String element=ir.next().toString();
 	    char dst[]=new char[20];
 	    char month[]=new char[20];
-		if(!(element.contains("ajax")))
-		{
-		url= new URL(element);
-		getMails();
-		}
-		else
-		{
 		element.getChars(20, 31, dst, 0);
 		StringBuilder tempString=new StringBuilder().append(dst);
 		element.getChars(24, 26, month, 0);
 		StringBuilder month1=new StringBuilder().append(month);
-		Storable store=new FileSystem(WebClient.expandUrl(url,tempString.toString()),month1.toString());
+		URL storeURL=new URL(element);
+		Storable store=new FileSystem(storeURL,month1.toString());
 		store.start();
-		}
 	  }
-	}	
-}
+	}
+}	
+
